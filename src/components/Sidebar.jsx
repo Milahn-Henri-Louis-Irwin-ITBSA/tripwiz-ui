41bf9319ba41668a8cbf50cc449f4a0d143cabc1
Milahn William Martin <66036885+milahnmartin@users.noreply.github.com>
2023-08-05 14:32:06 +0200

    Added Font

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,15 +5,15 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
     return null;
   }
   return (
-    <div className="h-[95dvh] w-[20rem] absolute left-5 top-5 z-[99999] bg-slate-300 rounded">
+    <div className="h-[95dvh] w-[20rem] absolute left-5 top-5 z-[99999] bg-slate-100 rounded-3xl">
       <div className="h-2/6 p-2">
-        <div className="h-[20px] flex items-center justify-start">
+        <div className="h-[10px] pt-2 flex items-center justify-start">
           <svg
             onClick={() => setShowSidebar(false)}
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
-            fill="#0896FC"
+            fill="black"
             className="bi bi-arrow-bar-left cursor-pointer"
             viewBox="0 0 16 16"
           >
@@ -25,13 +25,14 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
         </div>
         <div className="h-full flex items-center justify-center">
           <img
+            id="logo"
             src={Logo}
             alt="logo"
-            className="w-[13rem] rounded-full aspect-sqaure object-cover"
+            className="w- rounded-full aspect-sqaure object-cover"
           />
         </div>
       </div>
-      <div className="h-4/6 flex flex-col justify-start items-center border-2 py-4 px-2 font-bold">
+      <div className="h-4/6 flex flex-col justify-start items-center py-4 px-2 font-bold">
         <SidebarItem name="HOME" />
       </div>
     </div>
