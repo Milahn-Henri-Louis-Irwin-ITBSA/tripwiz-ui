f032536dcc941f01abfe213aee020a2610f683df
Milahn William Martin <66036885+milahnmartin@users.noreply.github.com>
2023-08-05 11:01:05 +0200

    init

---
+++ src/views/Map.jsx
@@ -0,0 +1,91 @@
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+
+import { divIcon, Icon, point } from 'leaflet';
+
+// create custom icon
+const customIcon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/447/447031.png',
+  // eslint-disable-next-line no-undef
+  // iconUrl: require('./icons/download.png.png'),
+  iconSize: [38, 38], // size of the icon
+});
+
+// custom cluster icon
+const createClusterCustomIcon = function(cluster) {
+  return new divIcon({
+    html: `<span class='cluster-icon'>${cluster.getChildCount()}</span>`,
+    className: 'custom-marker-cluster',
+    iconSize: point(33, 33, true),
+  });
+};
+
+// markers
+const markers = [
+  {
+    geocode: [-25.731340, 28.218370],
+    popUp: 'Hi Im Pretoria',
+  },
+  {
+    geocode: [48.85, 2.3522],
+    popUp: 'Hello, I am pop up 2',
+  },
+  {
+    geocode: [48.855, 2.34],
+    popUp: 'Hello, I am pop up 3',
+  },
+];
+
+const initialMapCoordinates = [-28.4792625, 24.6727135];
+
+export default function Map() {
+  return (
+    <MapContainer center={initialMapCoordinates} zoom={6} className='w-screen h-screen'>
+      {/* OPEN STREEN MAPS TILES */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      {/* WATERCOLOR CUSTOM TILES */}
+      {/* <TileLayer
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+      /> */}
+      {/* GOOGLE MAPS TILES */}
+      {/* <TileLayer
+        attribution="Google Maps"
+        // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
+        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+        url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+        maxZoom={20}
+        subdomains={["mt0", "mt1", "mt2", "mt3"]}
+      /> */}
+
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the markers */}
+        {markers.map((marker, indx) => (
+          // eslint-disable-next-line react/jsx-key
+          <Marker position={marker.geocode} key={indx} icon={customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+
+        {/* Hard coded markers */}
+        {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
+          <Popup>This is popup 1</Popup>
+        </Marker>
+        <Marker position={[51.504, -0.1]} icon={customIcon}>
+          <Popup>This is popup 2</Popup>
+        </Marker>
+        <Marker position={[51.5, -0.09]} icon={customIcon}>
+          <Popup>This is popup 3</Popup>
+        </Marker>
+       */}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
