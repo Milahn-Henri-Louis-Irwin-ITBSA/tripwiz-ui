28a4c89030ac805da87fdf67f6e51270d3711381
Milahn William Martin <66036885+milahnmartin@users.noreply.github.com>
2023-08-05 13:38:26 +0200

    Fixed Create User

--- src/components/ui/UserInformation.jsx
+++ src/components/ui/UserInformation.jsx
@@ -2,6 +2,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/utils/firebase-config';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
+import Logo from '@/icons/SidebarLogo.png';
 export default function UserInformation() {
   const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
@@ -13,10 +14,10 @@ export default function UserInformation() {
 
   return (
     <img
-      src={user?.photoURL}
+      src={user?.photoURL || Logo}
       alt={user?.displayName}
       onClick={() => auth.signOut()}
-      className="w-12 aspect-square rounded-full absolute right-0 top-0 z-[9999] mr-4 mt-4 border-2 border-[#0896FC]"
+      className="w-14 aspect-square rounded-full absolute object-cover right-0 top-0 z-[9999] mr-4 mt-4 border-2 border-[#0896FC] cursor-pointer hover:scale-110  hover:rotate-180 transition-all duration-300"
     />
   );
 }
