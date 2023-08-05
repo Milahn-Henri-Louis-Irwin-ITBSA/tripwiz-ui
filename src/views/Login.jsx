525c5a2f470f92141c4a7933add5a9b465940071
Henri Louw <henri.louw@frei.one>
2023-08-05 14:35:12 +0200

    Added Card

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -71,51 +71,53 @@ export default function Login() {
 
   return (
     <div className="w-screen h-screen flex">
-      <div className="w-2/3 h-screen flex items-center justify-center">
+      <div className="w-2/3 h-screen flex items-center justify-center ">
         <img src={logo} alt="logo" className="w-[20rem]" />
       </div>
-      <div className="w-1/3 flex flex-col items-center justify-center gap-4 px-4">
-        <span className="flex flex-col w-full gap-2">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            value={email}
-            placeholder="milahn@gmail.com"
-            className="outline-none border-b border-[#0896FC] px-4 py-2"
-            onChange={({ target }) => setEmail(target.value.trim())}
-          />
-        </span>
-        <span className="flex flex-col w-full gap-2">
-          <label htmlFor="email">Password</label>
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            className="outline-none border-b border-[#0896FC] px-4 py-2"
-            onChange={({ target }) => setPassword(target.value.trim())}
-          />
-        </span>
-        <button
-          onClick={handleEmailAndPasswordSignIn}
-          disabled={isButtonDisabled()}
-          className="w-full bg-[#0896FC] text-white px-4 py-2 rounded"
-        >
-          {loading ? (
-            <LoadingIndicator loadingMessage="Validating..." />
-          ) : (
-            'SIGN IN'
-          )}
-        </button>
-        {/* divider */}
-        <div className="flex items-center gap-4">
-          <span>OR</span>
+      <div className="w-1/3  flex items-center justify-center ">
+        <div className="shadow-2xl rounded-lg w-[75%] h-[50%] flex flex-col items-center justify-center gap-4 px-4">
+          <span className="flex flex-col w-full gap-2">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              value={email}
+              placeholder="milahn@gmail.com"
+              className="outline-none border-b border-[#0896FC] px-4 py-2"
+              onChange={({ target }) => setEmail(target.value.trim())}
+            />
+          </span>
+          <span className="flex flex-col w-full gap-2">
+            <label htmlFor="email">Password</label>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              className="outline-none border-b border-[#0896FC] px-4 py-2"
+              onChange={({ target }) => setPassword(target.value.trim())}
+            />
+          </span>
+          <button
+            onClick={handleEmailAndPasswordSignIn}
+            disabled={isButtonDisabled()}
+            className="w-full bg-[#0896FC] text-white px-4 py-2 rounded"
+          >
+            {loading ? (
+              <LoadingIndicator loadingMessage="Validating..." />
+            ) : (
+              'SIGN IN'
+            )}
+          </button>
+          {/* divider */}
+          <div className="flex items-center gap-4">
+            <span>OR</span>
+          </div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="w-full rounded bg-black px-4 py-2 flex items-center justify-center"
+          >
+            <img src={GoogleIcon} className="w-6" />
+          </button>
         </div>
-        <button
-          onClick={() => signInWithGoogle()}
-          className="w-full rounded bg-black px-4 py-2 flex items-center justify-center"
-        >
-          <img src={GoogleIcon} className="w-6" />
-        </button>
       </div>
     </div>
   );
