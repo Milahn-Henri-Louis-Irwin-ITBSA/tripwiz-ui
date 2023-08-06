907392dbce826ece229a82eab46f05deec36ee94
Henri Louw <henri.louw@frei.one>
2023-08-06 20:47:11 +0200

    Login Card Updates

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -77,7 +77,89 @@ export default function Login() {
       </div>
 
       <div className="w-1/3 h-[75%] flex items-center justify-center border-l-4 border-l-white  z-20  ">
-        <div className="shadow-2xl rounded-lg w-[75%] h-[50%] flex flex-col items-center justify-center gap-4 px-4 bg-white">
+        <div class="card">
+          <div class="card-details ">
+            <label
+              for="input-group-1"
+              class="block  text-base font-semibold text-gray-400"
+            >
+              Email
+            </label>
+            <div class="relative mb-2">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-[#0896FC] "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 16"
+                >
+                  <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                  <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                </svg>
+              </div>
+              <input
+                type="text"
+                value={email}
+                id="input-group-1"
+                className="bg-gray-50 input-border border-2 border-[#0896fc]  text-gray-800 font-semibold outline-none text-sm rounded-lg  block w-full pl-10 p-2.5"
+                placeholder="support@tripwhiz.com"
+                onChange={({ target }) => setEmail(target.value.trim())}
+              />
+            </div>
+            <label
+              for="input-group-1"
+              class="block  text-base font-semibold text-gray-400"
+            >
+              Password
+            </label>
+            <div class="relative mb-6">
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="w-4 h-4 text-[#0896FC] "
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+              </div>
+              <input
+                type="password"
+                value={password}
+                id="input-group-1"
+                className="bg-gray-50 input-border border-2 border-[#0896fc]  text-gray-800 font-semibold outline-none text-sm rounded-lg  block w-full pl-10 p-2.5"
+                placeholder="support@tripwhiz.com"
+                onChange={({ target }) => setPassword(target.value.trim())}
+              />
+            </div>
+
+            <button
+              onClick={handleEmailAndPasswordSignIn}
+              disabled={isButtonDisabled()}
+              className="w-full bg-[#0896FC] text-white px-4 py-2 rounded"
+            >
+              {loading ? (
+                <LoadingIndicator loadingMessage="Validating..." />
+              ) : (
+                'SIGN IN'
+              )}
+            </button>
+
+            <div className="flex items-center justify-center gap-4">
+              <span className="text-gray-400 font-semibold">OR</span>
+            </div>
+            <button
+              onClick={() => signInWithGoogle()}
+              className="w-full rounded bg-black px-4 py-2 flex items-center justify-center"
+            >
+              <img src={GoogleIcon} className="w-6" />
+            </button>
+          </div>
+        </div>
+        {/* <div className="shadow-2xl rounded-lg w-[75%] h-[50%] flex flex-col items-center justify-center gap-4 px-4 bg-white">
           <span className="flex flex-col w-full gap-2">
             <label htmlFor="email">Email</label>
             <input
@@ -119,7 +201,7 @@ export default function Login() {
           >
             <img src={GoogleIcon} className="w-6" />
           </button>
-        </div>
+        </div> */}
       </div>
 
       <svg
