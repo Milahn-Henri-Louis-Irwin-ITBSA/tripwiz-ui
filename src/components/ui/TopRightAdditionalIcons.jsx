8077ef2d2d0969a7422d2c45144cb7c304ea382a
Henri Louw <henri.louw@frei.one>
2023-11-11 11:17:33 +0200

    chore(Multiple Files):Expand Icons

--- src/components/ui/TopRightAdditionalIcons.jsx
+++ src/components/ui/TopRightAdditionalIcons.jsx
@@ -1,24 +1,17 @@
+import ExpandLeft from '../../assets/ExpandLeft.png';
 export default function TopLeftAdditionalIcons({ showFeed, setShowFeed }) {
   if (showFeed) {
     return null;
   }
 
   return (
-    <div className="absolute top-24 right-6 z-[9999]">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        fill="currentColor"
-        className="bi bi-arrow-left-square cursor-pointer"
-        viewBox="0 0 16 16"
+    <div className="absolute top-24 right-5 z-[9999]">
+      <img
+        className="w-10 cursor-pointer"
+        src={ExpandLeft}
+        alt="Expand Menu"
         onClick={() => setShowFeed(true)}
-      >
-        <path
-          fillRule="evenodd"
-          d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
-        />
-      </svg>
+      />
     </div>
   );
 }
