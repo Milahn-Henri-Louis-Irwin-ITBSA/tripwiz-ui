5bcae4ad886e7aa1a89c1ae9cf5732c706d75779
Henri Louw <henri.louw@frei.one>
2023-11-15 16:19:30 +0200

    fix(Sidebar.jsx):Removed Routing & Added Fuel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,6 +17,7 @@ export default function Sidebar({
   const [kmValues, setKmValues] = useState({
     kmValue: 1,
     kmValueFlights: 1,
+    kmValueFuel: 1,
   });
   const [eventCounts, setEventCounts] = useState({
     fire: 0,
@@ -101,8 +102,14 @@ export default function Sidebar({
     return null;
   }
 
-  const { kmValue, kmValueFlights } = kmValues;
+  const { kmValue, kmValueFlights, kmValueFuel } = kmValues;
 
+<<<<<<< Updated upstream
+=======
+  // console.log('Data is: ', data);
+  // console.log('Places is: ', data[0].places);
+  // console.log('Location is: ', data[0].places[0].location);
+>>>>>>> Stashed changes
   return (
     <div className="h-[95vh] w-[20rem] absolute left-5 top-5 z-[99999] bg-slate-100 rounded-3xl shadow-xl">
       <div className="h-1/5 pt-5 px-5 mb-6">
@@ -129,7 +136,7 @@ export default function Sidebar({
         </div>
       </div>
       <div className="h-4/5 flex flex-col justify-start items-center pb-4 px-2 font-bold">
-        <div className="flex gap-5">
+        <div className="flex gap-5 mb-6">
           <div className="text-center">
             <img src={pinFire} alt="Fire" className="w-9 mb-1" />
             <p className="text-blue-500 font-semibold text-lg">
@@ -167,68 +174,48 @@ export default function Sidebar({
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
-              className="bi bi-globe-americas w-7 text-gray-600"
+              className="bi bi-fuel-pump w-7 text-gray-600"
               viewBox="0 0 16 16"
             >
-              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z" />
+              <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-5Z" />
+              <path d="M1 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v8a2 2 0 0 1 2 2v.5a.5.5 0 0 0 1 0V8h-.5a.5.5 0 0 1-.5-.5V4.375a.5.5 0 0 1 .5-.5h1.495c-.011-.476-.053-.894-.201-1.222a.97.97 0 0 0-.394-.458c-.184-.11-.464-.195-.9-.195a.5.5 0 0 1 0-1c.564 0 1.034.11 1.412.336.383.228.634.551.794.907.295.655.294 1.465.294 2.081v3.175a.5.5 0 0 1-.5.501H15v4.5a1.5 1.5 0 0 1-3 0V12a1 1 0 0 0-1-1v4h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V2Zm9 0a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13h8V2Z" />
             </svg>
           </div>
         </div>
 
-        <div className="flex mb-4">
-          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-[#005DCA] border-2 border-[#005DCA] rounded-e-0  rounded-s-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              className="bi bi-geo w-4 text-white"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"
-              />
-            </svg>
-          </span>
+        <div className="mb-4">
+          <label htmlFor="km-range" className="block text-gray-700 mb-1">
+            Select a KM Range:{' '}
+            <span className="text-blue-500 font-semibold ms-1">
+              {kmValueFuel} km
+            </span>
+          </label>
           <input
-            onInput={(e) => setStart(e.target.value)}
-            type="text"
-            id="starting-location"
-            className="rounded-none rounded-e-lg bg-gray-50 border text-gray-900  block flex-1 min-w-0 w-full text-sm p-2.5  focus:outline-none "
-            placeholder="Starting Location"
+            type="range"
+            id="km-range"
+            name="kmValue"
+            min="1"
+            max="100"
+            value={kmValues.kmValueFuel}
+            onChange={handleRangeChange}
+            className="w-full h-4 bg-gray-300 rounded-full appearance-none focus:outline-none"
           />
         </div>
-        <div className="flex mb-4">
-          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-[#005DCA] border-2 border-[#005DCA] rounded-e-0  rounded-s-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              className="bi bi-geo-fill w-4 text-white"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"
-              />
-            </svg>
-          </span>
-          <input
-            onInput={(e) => setEnd(e.target.value)}
-            type="text"
-            id="ending-location"
-            className="rounded-none rounded-e-lg bg-gray-50 border text-gray-900  block flex-1 min-w-0 w-full text-sm p-2.5  focus:outline-none "
-            placeholder="Destination Location"
-          />
-        </div>
-        <button className="bg-[#005DCA] text-white p-3  rounded-md flex items-center justify-center w-[16.25rem]">
+
+        <button
+          className="bg-[#005DCA] text-white p-3 rounded-md flex items-center justify-center w-[16.25rem]"
+          onClick={async () => await fetchTourismData('fuel', kmValueFuel)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
-            className="bi bi-globe-americas w-5 me-2"
+            className="bi bi-fuel-pump w-5 text-white me-2"
             viewBox="0 0 16 16"
           >
-            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z" />
+            <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-5Z" />
+            <path d="M1 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v8a2 2 0 0 1 2 2v.5a.5.5 0 0 0 1 0V8h-.5a.5.5 0 0 1-.5-.5V4.375a.5.5 0 0 1 .5-.5h1.495c-.011-.476-.053-.894-.201-1.222a.97.97 0 0 0-.394-.458c-.184-.11-.464-.195-.9-.195a.5.5 0 0 1 0-1c.564 0 1.034.11 1.412.336.383.228.634.551.794.907.295.655.294 1.465.294 2.081v3.175a.5.5 0 0 1-.5.501H15v4.5a1.5 1.5 0 0 1-3 0V12a1 1 0 0 0-1-1v4h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V2Zm9 0a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13h8V2Z" />
           </svg>
-          <span className="text-sm">Start Navigation</span>
+          <span className="text-sm">Show Fuel Stations</span>
         </button>
 
         <div className="inline-flex items-center justify-center w-full">
