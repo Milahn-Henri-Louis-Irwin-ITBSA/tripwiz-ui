6a2e865eb5a8d74597b8d67236bc176af88dedf0
Henri Louw <henri.louw@frei.one>
2023-11-19 12:19:13 +0200

    Sidebar Changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -146,11 +146,9 @@ export default function Sidebar({
     return null;
   }
 
-  // const { kmValue, kmValueFlights, kmValueFuel } = kmValues;
-
   return (
     <div className="h-[95vh] w-[20rem] absolute left-5 top-5 z-[99999] bg-slate-100 rounded-3xl shadow-xl">
-      <div className="h-1/5 pt-5 px-5 mb-6">
+      <div className="h-1/6 pt-3 px-5 mb-6">
         <div className="h-[10px] pt-2 flex items-center justify-start">
           <svg
             onClick={() => setShowSidebar(false)}
@@ -164,12 +162,12 @@ export default function Sidebar({
             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
           </svg>
         </div>
-        <div className="h-36 flex items-center justify-center ">
+        <div className=" flex items-center justify-center ">
           <img
             id="logo"
             src={Logo}
             alt="logo"
-            className=" rounded-full aspect-sqaure w-56"
+            className=" rounded-full aspect-sqaure w-40"
           />
         </div>
       </div>
